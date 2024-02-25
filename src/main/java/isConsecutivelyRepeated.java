61831a7e70b8c634f3d7d6793c407b7008433d4e
Noam Miller <126347535+ThatsNoam@users.noreply.github.com>
2024-02-25 09:44:09 +0200

    Add files via upload

---
+++ src/main/java/isConsecutivelyRepeated.java
@@ -0,0 +1,38 @@
+
+public class isConsecutivelyRepeated {
+	//פעולה שמקבלת תור ומספר ומחזירה האם הוא מופיע יותר מפעמיים ברצף
+	public static boolean isConsecutivelyRepeated(Queue<Integer> q,int number) {
+		boolean flag = false;
+		Queue<Integer> q1 = new Queue<>();
+		while (!q.isEmpty()) {
+			if (q.head() == number ) {
+				q1.insert(q.remove());
+				if (!q.isEmpty()) {
+					if (q.head() == number) {
+						flag = true;
+					}				
+				}
+			}
+			else {
+			q1.insert(q.remove());
+			}
+		}
+		while (!q1.isEmpty()) {
+			q.insert(q1.remove());
+		}
+		return flag;
+	}
+	public static void main(String[] args) {
+		Queue<Integer> q1 = new Queue<>();
+		q1.insert(1);
+		
+		
+		
+		System.out.println(q1);
+		boolean a = (isConsecutivelyRepeated(q1,1));
+		System.out.println(a);
+		System.out.println(q1);
+		
+	}
+
+}
